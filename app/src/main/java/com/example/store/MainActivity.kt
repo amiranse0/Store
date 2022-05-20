ae2c43cb58f833d90dafcdbbfb922c0d266e8921
amirabbasnesaee <nesaee@ce.sharif.edu>
2022-05-20 13:08:55 -0700

    splash screen added

--- app/src/main/java/com/example/store/MainActivity.kt
+++ app/src/main/java/com/example/store/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.store
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationSet
+import com.example.store.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        splashHandling()
+    }
+
+    private fun splashHandling() {
+        binding.shoppingBag.alpha = 0f
+        binding.shoppingBag.animate().setDuration(3000).alpha(1f).withEndAction{
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
     }
 }
