feb11e54a260db9b95d11b16c83b52919a689e14
amirabbasnesaee <nesaee@ce.sharif.edu>
2022-05-20 22:56:16 -0700

    result sealed class added

---
+++ app/src/main/java/com/example/store/data/Repository.kt
@@ -0,0 +1,8 @@
+package com.example.store.data
+
+import com.example.store.data.source.IStoreService
+import javax.inject.Inject
+
+class Repository @Inject constructor(service: IStoreService) {
+
+}
--- app/src/main/java/com/example/store/data/source/RemoteDataSource.kt
+++ app/src/main/java/com/example/store/data/source/RemoteDataSource.kt
@@ -36,6 +36,4 @@ class RemoteDataSource(
         bestQuery["page"] = "$page"
         return service.getProduct(bestQuery)
     }
-
-
 }
