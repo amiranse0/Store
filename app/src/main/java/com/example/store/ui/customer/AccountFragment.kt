2d8621f9ef1bc8b8d2fc11a1ea0173011566735a
amirabbasnesaee <nesaee@ce.sharif.edu>
2022-06-09 23:05:01 +0430

    layout for customer added

--- app/src/main/java/com/example/store/ui/customer/AccountFragment.kt
+++ app/src/main/java/com/example/store/ui/customer/AccountFragment.kt
@@ -1,10 +1,9 @@
-package com.example.store.ui.product.account
+package com.example.store.ui.customer
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.store.R
-import com.example.store.data.connection.NetworkConnection
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
--- app/src/main/java/com/example/store/ui/customer/CustomerFragment.kt
+++ app/src/main/java/com/example/store/ui/customer/CustomerFragment.kt
@@ -1,9 +1,20 @@
 package com.example.store.ui.customer
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.store.R
+import com.example.store.databinding.FragmentCategoryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CustomerFragment: Fragment(R.layout.fragment_customer) {
+
+    private lateinit var binding: FragmentCategoryBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCategoryBinding.bind(view)
+
+    }
 }
